f1dfa5c1a855dff08a6b05a82add7a41fecc5d50
shl9012 <zoflrjs2145@naver.com>
2021-03-30 20:30:23 +0900

    devus-be-api 프로젝트 빌드업

---
+++ devus-be/devus-api/src/test/java/com/hst/devus/DevusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hst.devus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
