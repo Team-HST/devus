8dc4b1f0416998c72f510bcaca8b4006b6023307
HyunGyu-Lee <gusrb0808@naver.com>
2021-06-10 19:20:58 +0900

    설정파일 변경

---
+++ devus-be/devus-config/src/main/java/com/hst/devus/config/Test.java
@@ -0,0 +1,8 @@
+package com.hst.devus.config;/**
+ * Copyright 2021 NHN Corp. All rights Reserved.
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ * @author hyungyu.lee@nhn.com
+ * @date 2021-05-24
+ */public class Test {
+}
